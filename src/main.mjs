fc7f9c5b42ec798a3c862175212d97c43e0cd2d8
avix133 <avix133@gmail.com>
2025-04-13 21:27:08 +0200

    fix: settings are not persistent through character reload

--- src/main.mjs
+++ src/main.mjs
@@ -32,52 +32,53 @@ function progressiveOfflineSpeedup(multiplicationPercentByElapsedHour, enabledNo
 
 
 export function setup(ctx) {
+
+    let settingsSpeedupStages = ctx.settings.section('Speedup stages');
+
+    settingsSpeedupStages.add([
+        {
+            type: 'number',
+            name: 'progressive-offline-speedup-1h-multiplier',
+            label: 'After 1 hour (%)',
+            default: 120
+        },
+        {
+            type: 'number',
+            name: 'progressive-offline-speedup-2h-multiplier',
+            label: 'After 2 hours (%)',
+            default: 150
+        },
+        {
+            type: 'number',
+            name: 'progressive-offline-speedup-4h-multiplier',
+            label: 'After 4 hours (%)',
+            default: 200
+        },
+        {
+            type: 'number',
+            name: 'progressive-offline-speedup-8h-multiplier',
+            label: 'After 8 hours (%)',
+            default: 250
+    }]);
+
+
+    let settingsGeneral = ctx.settings.section('General');
+
+    settingsGeneral.add({
+        type: 'switch',
+        name: 'enable-progressive-offline-speedup',
+        label: 'Enable Progressive Offline Speedup',
+        default: true
+    });
+
+    settingsGeneral.add({
+        type: 'switch',
+        name: 'progressive-offline-speedup-enable-notification',
+        label: 'Enable notification',
+        default: false
+    });
+
     ctx.onCharacterLoaded(ctx => {
-
-        let settingsGeneral = ctx.settings.section('General');
-
-        settingsGeneral.add({
-            type: 'switch',
-            name: 'enable-progressive-offline-speedup',
-            label: 'Enable Progressive Offline Speedup',
-            default: true
-        });
-
-        settingsGeneral.add({
-            type: 'switch',
-            name: 'progressive-offline-speedup-enable-notification',
-            label: 'Enable notification',
-            default: false
-        });
-
-        let settingsSpeedupStages = ctx.settings.section('Speedup stages');
-
-        settingsSpeedupStages.add([
-            {
-                type: 'number',
-                name: 'progressive-offline-speedup-1h-multiplier',
-                label: 'After 1 hour (%)',
-                default: 120
-            },
-            {
-                type: 'number',
-                name: 'progressive-offline-speedup-2h-multiplier',
-                label: 'After 2 hours (%)',
-                default: 150
-            },
-            {
-                type: 'number',
-                name: 'progressive-offline-speedup-4h-multiplier',
-                label: 'After 4 hours (%)',
-                default: 200
-            },
-            {
-                type: 'number',
-                name: 'progressive-offline-speedup-8h-multiplier',
-                label: 'After 8 hours (%)',
-                default: 250
-        }]);
-    
         function run() {
             if (settingsGeneral.get('enable-progressive-offline-speedup') == false) {
                 console.log('Progressive offline speedup is disabled');
